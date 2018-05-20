984d41f944d992be8dbda827c592d74ea8c62ed1
vermaboys <vermaboys9@gmail.com>
2018-05-20 11:29:51 +0530

    initial commit

--- public/js/custom.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	
-	/*if ((screen.width>500))  { 
-		var linkHeight = $(".Links1").height();
-		$(".quickLinks").height( linkHeight ) - $("#worldNews").height();
-	}*/
-	
-})
